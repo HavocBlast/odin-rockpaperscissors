4e1d345adfc378385e69353c9b0fcc31ec55cb3a
Jacob Kruse <jacob.kruse@bananaclip.studio>
2022-11-18 01:31:48 -0500

    Creates UI using the DOM

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let playerWins = 0;
 let computerWins = 0;
-
 function getComputerChoice() {
   let computerChoiceNum = Math.floor(Math.random() * 3);
-
+  //selects rock, paper, or scissor based on the random computerChoiceNum value
   switch (computerChoiceNum) {
     case 0:
       return 'rock';
@@ -22,59 +21,53 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-  if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'paper') {
+  if (playerChoice === 'rock' && computerChoice === 'paper') {
     computerWins++;
     return 'You lose! Paper covers Rock';
   }
-  if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'scissor') {
+  if (playerChoice === 'paper' && computerChoice === 'scissor') {
     computerWins++;
     return 'You lose! Scissor cuts Paper';
   }
-  if (
-    playerChoice.toLowerCase() === 'scissor' ||
-    (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'rock')
-  ) {
+  if (playerChoice === 'scissor' && computerChoice === 'rock') {
     computerWins++;
     return 'You lose! Rock crushes Scissors';
   }
-  if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'scissor') {
+  if (playerChoice === 'rock' && computerChoice === 'scissor') {
     playerWins++;
     return 'You win! Rock crushes Scissors';
   }
-  if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'rock') {
+  if (playerChoice === 'paper' && computerChoice === 'rock') {
     playerWins++;
     return 'You win! Paper covers Rock';
   }
-  if (
-    playerChoice.toLowerCase() === 'scissor' ||
-    (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'paper')
-  ) {
+  if (playerChoice === 'scissor' && computerChoice === 'paper') {
     playerWins++;
     return 'You win! Scissor cuts Paper';
   }
-  if (playerChoice.toLowerCase() === 'rock' && computerChoice === 'rock') {
+  if (playerChoice === 'rock' && computerChoice === 'rock') {
     return 'You Draw!';
   }
-  if (playerChoice.toLowerCase() === 'paper' && computerChoice === 'paper') {
+  if (playerChoice === 'paper' && computerChoice === 'paper') {
     return 'You Draw!';
   }
-  if (
-    playerChoice.toLowerCase() === 'scissor' ||
-    (playerChoice.toLowerCase() === 'scissors' && computerChoice === 'scissor')
-  ) {
+  if (playerChoice === 'scissor' && computerChoice === 'scissor') {
     return 'You Draw!';
   } else {
     console.error('Please enter a valid choice "Rock", "Paper", or "Scissors"');
   }
 }
 
-function game() {
-  for (let game = 0; game < 5; game++) {
-    let playerChoice = prompt('Pick "Rock", "Paper" or "Scissors"');
+function game(playerChoice) {
+  if (playerWins === 5 || computerWins === 5) {
+    //game is over and show the winner
+    const html = document.getElementById('results');
+    html.innerHTML = `The ${playerWins === 5 ? 'player' : 'computer'} wins!`;
+  } else {
     let computerChoice = getComputerChoice();
-    let result = playRound(playerChoice, computerChoice);
-    console.log(result);
-    
+    const htmlScore = document.getElementById('score');
+    const html = document.getElementById('results');
+    html.innerHTML = playRound(playerChoice, computerChoice);
+    htmlScore.innerHTML = `Player: ${playerWins} Computer: ${computerWins}`;
   }
-  console.log(`Game ended!  Player: ${playerWins} Computer: ${computerWins}`)
 }
